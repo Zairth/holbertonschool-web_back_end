0d618c10e0b100db6b26d8efe914b87b15b1b07e
Zairth <mano.gimenez@hotmail.com>
2025-03-26 16:54:16 +0100

    gg

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   let idx = 0;
   for (const value of array) {
+    // eslint-disable-next-line
     array[idx] = appendString + value;
     idx += 1;
   }
