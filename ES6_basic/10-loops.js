9f0f78c54709720bf21a59df65f8ebd20def8f95
Zairth <mano.gimenez@hotmail.com>
2025-03-26 16:45:01 +0100

    gg

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,3 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  return array.map((value) => appendString + value);
+  let idx = 0;
+  const newArray = [...array];
+  for (const value of newArray) {
+    newArray[idx] = appendString + value;
+    idx += 1;
+  }
+  return newArray;
 }
