11bc0ed1ee5ab1af8372d34de07f55c39ea1bc92
Zairth <mano.gimenez@hotmail.com>
2025-04-01 10:36:27 +0200

    gg

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,7 @@
 export default function getStudentIdsSum(listOfStudents) {
+  if (!Array.isArray(listOfStudents)) {
+    throw new TypeError('arg must be an array');
+  }
   return listOfStudents.reduce(
     (accumulator, currentValue) => accumulator + currentValue.id, 0,
   );
