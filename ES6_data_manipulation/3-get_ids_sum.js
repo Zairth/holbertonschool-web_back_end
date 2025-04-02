d27256f200c26f85c584fdf28373c2957d191896
Zairth <mano.gimenez@hotmail.com>
2025-04-02 17:37:34 +0200

    gg

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -2,7 +2,5 @@ export default function getStudentIdsSum(listOfStudents) {
   if (!Array.isArray(listOfStudents)) {
     return 0;
   }
-  return listOfStudents.reduce(
-    (accumulator, currentValue) => accumulator + currentValue.id, 0,
-  );
+  return listOfStudents.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0);
 }
