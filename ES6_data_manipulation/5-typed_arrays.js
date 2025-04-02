a6eee494ed0ce7206a7a39889b6f8cc48eb2f556
Zairth <mano.gimenez@hotmail.com>
2025-04-02 18:30:32 +0200

    gg

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,15 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (typeof length !== 'number' || typeof position !== 'number' || typeof value !== 'number') {
+    throw new TypeError('Args must be numbers');
+  }
+
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  view.setUint8(position, value);
+
+  return view;
+}
