0937fe05402e87c1fa73850b5b3d590135efca28
Zairth <mano.gimenez@hotmail.com>
2025-04-03 15:08:04 +0200

    gg

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,20 @@
 export default function cleanSet(set, startString) {
   const iterator = set.values();
-  let newString = '';
 
-  for (let element of iterator) {
-    element = String(element);
-    if (element.startsWith(startString)) {
-      newString += element.replace(startString, '');
-      newString += '-';
+  const len = startString.trim().length;
+
+  if (len !== 0) {
+    let newString = '';
+    for (let element of iterator) {
+      element = String(element);
+      if (element.startsWith(startString)) {
+        newString += element.replace(startString, '');
+        newString += '-';
+      }
     }
-  }
 
-  newString = newString.slice(0, -1);
-  return newString;
+    newString = newString.slice(0, -1);
+    return newString;
+  }
+  return startString;
 }
