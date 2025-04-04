766b6f43d93807b6e65018da82fd8d2b4e00f124
Zairth <mano.gimenez@hotmail.com>
2025-04-04 10:56:49 +0200

    gg

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,8 +4,8 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => results
-      .forEach((result) => [{
+      .map((result) => ({
         status: result.status,
         value: result.value,
-      }]));
+      })));
 }
