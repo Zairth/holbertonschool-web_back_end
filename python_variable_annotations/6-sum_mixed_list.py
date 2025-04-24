5d90690da08b7d9034bfcc60834f0acf6baf1604
Zairth <mano.gimenez@hotmail.com>
2025-04-24 12:08:14 +0200

    gg

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""6-sum_mixed_list.py"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """return the sum of a list of int and float, as a float"""
+    total = 0
+    for value in mxd_lst:
+        total += value
+    return total
