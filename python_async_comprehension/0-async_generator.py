6d01a445aac0e0a4154b6bba24983e6bef36ff40
Zairth <mano.gimenez@hotmail.com>
2025-04-28 12:03:14 +0200

    gg

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,8 +8,6 @@ async def async_generator():
     """
     Write a coroutine called async_generator that takes no arguments
     """
-    list = []
     for i in range(10):
         await asyncio.sleep(1)
-        list.append(random.uniform(0, 10))
-    return list
+        yield random.uniform(0, 10)
