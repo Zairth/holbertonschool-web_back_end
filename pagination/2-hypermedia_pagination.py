5a6294096386cde739f0e51f41e3e52bed338f7f
Zairth <mano.gimenez@hotmail.com>
2025-04-30 12:27:25 +0200

    gg

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""1-simple_pagination.py"""
+from typing import Tuple
+import csv
+from typing import List
+import math
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    The function should return a tuple of size two containing
+    a start index and an end index corresponding to the range
+    of indexes to return in a list for those particular
+    pagination parameters
+    """
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get page
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        datas = self.dataset()
+        start, end = index_range(page, page_size)
+        return datas[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Get Hypermedia
+        """
+        hyper_data = self.get_page(page, page_size)
+        all_datas = self.dataset()
+
+        next_page = page + 1 if len(self.get_page(page + 1, page_size)) \
+            > 0 else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            "page_size": len(hyper_data),
+            "page": page,
+            "data": hyper_data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_page": math.ceil(len(all_datas) / page_size)
+        }
